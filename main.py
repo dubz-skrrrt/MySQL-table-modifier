61443e84f0ef94aa45796077c23f19e777a867f7
dubz-skrrrt <chill.99.lr@gmail.com>
2022-05-04 15:01:00 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from tkinter import *
+from tkinter import ttk
+import pandas as pd
+import xlrd
+
+window = Tk()
+window.title("MySQL Table Modifier")
+window.geometry("1080x720")
+
+window.mainloop()
+
+def Choosing_Database():
+    #code
+    print()
+
+def Choosing_Table():
+    #code
+    print()
+
+
