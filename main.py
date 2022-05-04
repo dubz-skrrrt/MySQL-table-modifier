3b5b14d66706506422e0c90fc54c6eba7fca28cc
dubz-skrrrt <chill.99.lr@gmail.com>
2022-05-04 19:45:03 +0800

    Added frame to tkinter

--- main.py
+++ main.py
@@ -28,72 +28,60 @@ def Choosing_Database():
 
     try:
         input_db.delete(0, END)
-        engine = create_engine("mysql://{0}:{1}@{2}:{3}/{4}".format(user, password, host, port, database))  # after//= user:password@localhost(or ip address)/database name
-        #lbl.config(text=f"Database: connection to the '{host}' for user '{user}' created successfully")
+        engine = create_engine("mysql://{0}:{1}@{2}:{3}/{4}".format(
+            user,
+            password,
+            host,
+            port,
+            database))  # after//= user:password@localhost(or ip address)/database name
+
+        lbl.config(text=f"Database: connection to the '{host}' for user '{user}' created successfully")
         r_set = engine.execute("SHOW TABLES")
-        Session = sessionmaker(bind=engine)
+
         create_table_list()
         print(f"connection to the {host} for user {user} created successfully")
     except SQLAlchemyError as e:
         print("Error in connecting to database due to the following error: ", e)
-    finally:
 
-        print()
-        # input_lbl_tbl = Label(window, text="Table: ")
-        # input_lbl_tbl.pack()
-        # input_tbl = tk.Text(window, height=1, width=20)
-        # input_tbl.pack()
 def create_table_list():
     i = 0
-    search_tbl = Entry(window, width=30)
+    search_tbl = Entry(table_frame, width=25)
     search_tbl.insert(0, 'search for a table...')
     search_tbl.grid(row=3, column=0)
-    search_btn = Button(window, text="Search", command=Choosing_Database)
+    search_btn = Button(table_frame, text="Search", command=Choosing_Database)
     search_btn.grid(row=3, column=1)
     title = [i for i in r_set.keys()]
     print(title)
-    list_tbl = Label(window, text=f"{title}", width=25, borderwidth=2, relief="ridge", anchor='w',
+    list_tbl = Label(table_frame, text=f"{title}", width=25, borderwidth=2, relief="ridge", anchor='w',
                      bg='green')
-    list_tbl.grid(row=4, column=0)
+    list_tbl.grid(row=4, column=0, pady=10)
 
     for data in r_set:
         for j in range(len(data)):
-            list_tables = Label(window, text=data[j], width=25, borderwidth=2, relief="ridge", anchor='w')
-            list_tables.grid(row=i + 5, column=j)
-            checkDB = Button(window, text="Update", command=Choosing_Table)
-            checkDB.grid(row=i + 5, column=j+1)
+            list_tables = Label(table_frame, text=data[j], width=25, borderwidth=2, relief="ridge", anchor='w')
+            list_tables.grid(row=i + 5, column=j, pady=2.5)
+            checkDB = Button(table_frame, text="Update", command=Choosing_Table)
+            checkDB.grid(row=i + 5, column=j+1, pady=2.5)
         i = i + 1
-    # inspector = inspect(engine)
-    # tables = inspector.get_table_names()
-    # total_rows = len(tables)
-    # total_colums = len(tables) - 1
-    #
-    # for dt in tables:
-    #     print(dt)
-    # for i in range(total_rows):
-    #     for j in range(total_colums):
-    #         print(j)
-    #         list_tbl = Label(window, text=tables[i], width=20, borderwidth=2, relief="ridge", anchor='w')
-    #         list_tbl.grid(row=i + 7, column=j)
-    #         checkDB = Button(window, text="Update", command=Choosing_Table)
-    #         checkDB.grid(row=i + 7, column=1)
-        # list_tbl.insert(END, tables[i])
+
 def Choosing_Table():
     # code
 
     print()
 
 input_lbl_db = Label(window, text="Database: ")
-input_lbl_db.grid(row=0, column=0)
+input_lbl_db.pack()
 input_db = Entry(window, width=30)
-input_db.grid()
+input_db.pack()
 
 checkDB = Button(window, text="Submit", command=Choosing_Database)
-checkDB.grid()
+checkDB.pack()
 
 lbl = Label(window, text="")
-lbl.grid()
+lbl.pack()
 
+table_frame = Frame(window)
+table_frame.pack()
 print(window.grid_size())
 #Choosing_Database()
 window.mainloop()
