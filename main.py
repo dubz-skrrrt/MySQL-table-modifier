74b0126ab204aaab9eba2aa50e428330e0ffc57f
dubz-skrrrt <chill.99.lr@gmail.com>
2022-05-05 19:27:08 +0800

    recoded the flow of the program

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.query import _column_descriptions
 import tkinter as tk
 from tkinter import *
 from tkinter.messagebox import showinfo
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import pandas as pd
 import xlrd
 
@@ -39,11 +37,11 @@ def Choosing_Database():
 
         #lbl.config(text=f"Database: connection to the '{host}' for user '{user}' created successfully")
         r_set = engine.execute("SHOW TABLES")
-
         create_table_list()
+        dbConnect_Top.destroy()
         print(f"connection to the {host} for user {user} created successfully")
-    except SQLAlchemyError and BaseException as e:
-        print("Error in connecting to database due to the following error: ", e)
+    except (SQLAlchemyError, BaseException) as e:
+        messagebox.showerror("Error in connecting to database due to the following error: ", e)
 
 def create_table_list():
     i = 0
@@ -51,7 +49,7 @@ def create_table_list():
     title = [i for i in r_set.keys()]
     print(title)
     list_tbl = Label(table_frame, text=f"{title}", width=25, borderwidth=2, relief="ridge", anchor='w',
-                     bg='green')
+                     bg='lightgray')
     list_tbl.grid(row=4, column=0, pady=10)
 
     for data in r_set:
@@ -63,15 +61,23 @@ def create_table_list():
             list_tables.grid(row=i + 5, column=j, pady=2.5)
 
             checkDB = Button(table_frame, text="Update Table")
-            checkDB.configure(command=lambda b=data[ind]: check_Table(b))
-            checkDB.grid(row=i + 5, column=j+1, pady=2.5, padx=50)
-            list_of_Tables.append(data[ind])
 
+            checkDB.grid(row=i + 5, column=j+1, padx=50)
+            if data[ind] not in list_of_Tables:
+                print(data[ind] not in list_of_Tables)
+                list_of_Tables.append(data[ind])
+                checkDB.configure(command=lambda b=data[ind]: check_Table(b))
+            else:
+                print(data[ind] not in list_of_Tables)
+                checkDB.configure(command=NONE)
             ind = ind+1
         i = i + 1
 
+
+
 def check_Table(num):
-    global selected_table
+    global selected_table, search_tbl, table
+    #menubar.add_cascade(label="Choose Table", command=create_table_list)
     for tables in list_of_Tables:
         if num == tables:
             table = tables
@@ -85,55 +91,98 @@ def Data_Table():
     tree["columns"] = headers
     tree["show"] = 'headings'
     ind = 0
+
+    delete_Table()
     for heading in headers:                         # Creates the headers for the treeview table
-        tree.column(heading, width=5, anchor='c')
+        tree.column(heading, width=15*len(headers), anchor='c')
         tree.heading(heading, text=heading)
         ind += 1
+
     data_ind = 0
     for data in selected_table:
+        print(data)
         tree.insert(parent="",
                     index='end',
                     iid=data[data_ind],
                     text=data[data_ind],
                     values=data[0:len(data)])       # Pass the data from mySQL table to the treeview table
         data_ind += 1
-    search_tbl = Entry(search_frame, width=25)
-    search_tbl.insert(0, 'search for id...')
-    search_tbl.grid(row=10, column=0)
-    search_btn = Button(search_frame, text="Search")
-    search_btn.grid(row=10, column=1)
+def singleData_Table():
+    id_details = search_tbl.get()
+    #print(id_details)
+    try:
+        for records in tree.get_children():
+            tree.delete(records)
+        single_data = engine.execute(f"""SELECT * FROM {table} WHERE id = {id_details}""")
+        results = single_data.fetchone()
+        print(results)
+        tree.insert(parent="",
+                    index=0,
+                    text=results,
+                    values=results[0:len(results)])
+    except (NameError, TypeError) as e:
+        print(f"Couldn't search due to the following error: {e}")
+        messagebox.showerror('Python Error', f"Couldn't search due to the following error: {e}")
 
 def selectDetails(items):
     curItem = tree.focus()
     print(tree.item(curItem, option='values'))      # returns the values of the clicked row
 
+def delete_Table():
+    x = tree.selection()
+    for records in x:
+        tree.delete(records)
+    for item in tree.get_children():
+        tree.delete(item)
 
-input_lbl_db = Label(window, text="Database: ")
-input_lbl_db.pack()
-input_db = Entry(window, width=20)
-input_db.pack()
+def open_Connector():
+    global input_db, dbConnect_Top
+    dbConnect_Top = Toplevel(window)
+    dbConnect_Top.geometry("250x200")
+    dbConnect_Top.title("Database Connector")
 
-checkDB = Button(window, text="Submit", command=Choosing_Database)
-checkDB.pack()
+    input_lbl_db = Label(dbConnect_Top, text="Database: ")
+    input_lbl_db.pack(pady=25)
+    input_db = Entry(dbConnect_Top, width=40)
+    input_db.pack(padx=25)
+
+    checkDB = Button(dbConnect_Top, text="Submit", command=Choosing_Database)
+    checkDB.pack(pady=10)
+
+menubar = Menu(window)
 
 lbl = Label(window, text="")
 lbl.pack()
 
-table_frame = Frame(window, borderwidth=2, relief='ridge')
-table_frame.pack()
 
-search_frame = Frame(window)
-search_frame.pack()
-data_table_frame = Frame(window, width=50)
+table_frame = Frame(window, borderwidth=2, relief='ridge')
+table_frame.pack(pady=25)
+
+search_frame = Frame(window, width=window.winfo_width())
+print(window.winfo_width())
+search_frame.pack(pady=25)
+
+search_tbl = Entry(search_frame, width=50)
+search_tbl.insert(0, 'search for id...')
+search_tbl.pack(side="left", padx=25)
+search_btn = Button(search_frame, text="Search ID", command=singleData_Table)
+search_btn.pack(side='right')
+
+data_table_frame = Frame(window, width=500)
 data_table_frame.pack()
-tree = ttk.Treeview(data_table_frame, selectmode='browse', height=5)
+
+tree = ttk.Treeview(data_table_frame, selectmode='browse', height=15)
 tree.pack(side="left")
+style = ttk.Style()
+style.theme_use("default")
 scroll = ttk.Scrollbar(data_table_frame, orient="vertical", command=tree.yview)
 scroll.pack(side="right", fill="y")
 tree.configure(yscrollcommand=scroll.set)
 tree.bind('<Motion>', 'break')
 tree.bind('<ButtonRelease-1>', selectDetails)
+menubar.add_cascade(label="Connect Database Engine", command=open_Connector)
 #Choosing_Database()
+window.config(menu=menubar)
 window.mainloop()
 
 
